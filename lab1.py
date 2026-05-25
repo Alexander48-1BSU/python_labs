d0b8b7381956efbe3b48453204d14030c23ff012
Alexander48-1BSU <trolleybus81alex@gmail.com>
2026-05-25 17:04:30 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,22 +2,23 @@
 import sys
 
 def odometer_generation(alphabet, k):
-    if k==0:
+    n = len(alphabet)
+    if k==0: #whether word length is equal to 0
         return []
         
-    indices= [0]*k #setting odometer to a zero
+    indices= [0]*k #creating and setting odometer to a zero
     result=[]
     
     while True:
         word="".join(alphabet[i] for i in indices) #converting digits to letters
         result.append(word) #adding to the list
         
-        pos= k-1#2-1=1 , moving odometer changing odometer's roller(digit wheel)
-        while pos>=0:
-            indices[pos]+=1 #C++
+        pos= k-1#2-1=1 , changing odometer's roller(digit wheel) one step back, during the first iteration to set it to the last position
+        while pos>=0: #if we still have space from left to move to
+            indices[pos]+=1 #moving the wheel of odometer
             if indices[pos]<n: #checking limits
-                break
-            else:
+                break #go back and save the word
+            else: # Did we run out of the alfabet?
                 indices[pos]=0
                 pos-=1 
         
@@ -28,8 +29,8 @@ def odometer_generation(alphabet, k):
 
 #alfabet creating
 alf =input('input sorted alphabet ')
-ncomb=input('input out word lenght ')
-n =len(alf)
+ncomb=int(input('input out word lenght '))
+
     
 if ncomb>10: 
     print('ncomb must be equal or less than 10')
