7812e82d3e4f75757eebf3b037cd84cf189a789e
Alexander48-1BSU <trolleybus81alex@gmail.com>
2026-05-25 17:07:11 +0300

    Update lab2.py

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 25 00:07:09 2026
 
-@author: robert
-"""
 import sys 
 from Bio.SeqUtils import nt_search # returns the searched sequence and all results including intersecting ones 
 
 with open("input2.txt", "r") as fl:
-    s=fl.readline().strip()
-    t=fl.readline().strip()
+    s=fl.readline().strip() #save the line with sequence
+    t=fl.readline().strip() #save the line with pattern
     
 if len(s) > 1000 or len(t) > 1000:
     print('the condition is not satified')
@@ -18,5 +12,5 @@ if len(s) > 1000 or len(t) > 1000:
     
 
 results=nt_search(s, t)
-final= [str(i) for i in results[1:]]
+final= [str(i) for i in results[1:]] #cut the pattern
 print(" ".join(final))
