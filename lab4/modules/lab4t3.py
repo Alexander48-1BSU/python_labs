a645a90db4f227667f0235469a3a4e1bf22331d6
Alexander48-1BSU <trolleybus81alex@gmail.com>
2026-05-29 01:20:31 +0300

    added more description

--- lab4/modules/lab4t3.py
+++ lab4/modules/lab4t3.py
@@ -1,25 +1,25 @@
 from Bio import SeqIO
-import textwrap
-import os
+import textwrap #shorten text
+import os  #import operational system to handle output folder creations
 
 def run3(fl):
-    # Establish the output directory structure
-    os.makedirs('output', exist_ok=True)
+    os.makedirs('output', exist_ok=True) #creates an output folder if it does not exist yet (in our case it will exist based on main.py structure)
 
-    # Initialize file streams for both reading and writing
     with open(fl, "r") as input_fl:
-        with open('output/lab4t3.txt', 'w', encoding='utf-8') as out_fl:
+        with open('output/lab4t3.txt', 'w', encoding='utf-8') as out_fl: #writes an output in standart encoding inside the output folder
             
-            for record in SeqIO.parse(input_fl, "genbank"): 
+            for record in SeqIO.parse(input_fl, "genbank"): #parsing throw genbank objects with the biopython function
                 for i in record.features: 
-                    if i.type == "CDS": 
+                    if i.type =="CDS":  #selecting coding sequences
+#---------------------------------------------------------------
+#                         pull out translation (qualifiers meaning = 'translation'.
+#                         qualifiers always return list, so pulling out the first object to avoid list datatype
+                        protein =i.qualifiers['translation'][0] 
+#----------------------------------------------------------------                       
+                        wrapped_protein = textwrap.fill(protein, width=66) #takes fisrt 66 aminoacids
                         
-                        # Extract structural annotations
-                        protein = i.qualifiers['translation'][0] 
-                        wrapped_protein = textwrap.fill(protein, width=66)
-                        
-                        # Concatenate the biological metadata and sequence into a single memory block
-                        output_block = (
+
+                        output_block = ( #creates an output using f strings
                             f"{record.id}: {record.description}\n"
                             f"the location={i.location}\n"
                             f"Translation results=\n"
